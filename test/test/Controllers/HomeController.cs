7b45aa7604869792721f8ef200a2286899167bf1
shilp <shilp@LAPTOP-BQI39UAE>
2017-01-17 22:17:12 +1300

    first build

---
+++ test/test/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+using test.Models;
+
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private ExcerciseRepository excercise = new ExcerciseRepository();
+
+        public ActionResult Index()
+        {
+            var excercises = excercise.ExcerciseRep.Get();
+            return View(excercises.ToList());
+        }
+        public ActionResult Create()
+        {
+            PopulateDepartmentsDropDownList();
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add( Excercise exc)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    excercise.ExcerciseRep.Insert(exc);
+                    excercise.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            PopulateDepartmentsDropDownList(exc.Id);
+            return View(exc);
+        }
+
+        private void PopulateDepartmentsDropDownList(object selectedExcercise = null)
+        {
+            var excQuery = excercise.ExcerciseRep.Get(
+                orderBy: q => q.OrderByDescending(d => d.ExcerciseDate));
+            ViewBag.DepartmentID = new SelectList(excQuery, "Id", "ExcerciseName", selectedExcercise);
+        }
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
---
+++ test/test/Models/ExcerciseRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace test.Models
+{
+    public class ExcerciseRepository : IDisposable
+    {
+        private TestContext context = new TestContext();
+        private GenericRepository<Excercise> excerciseRepository;
+       
+        public GenericRepository<Excercise> ExcerciseRep
+        {
+            get
+            {
+
+                if (this.excerciseRepository == null)
+                {
+                    this.excerciseRepository = new GenericRepository<Excercise>(context);
+                }
+                return excerciseRepository;
+            }
+        }
+
+       
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
